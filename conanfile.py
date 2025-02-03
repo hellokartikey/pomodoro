c254708777f4960a865767d3894ff1517f494490
Kartikey Subramanium <kartikey.sub@outlook.com>
2025-02-03 22:34:07 +0530

    Move to conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,20 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+
+class Pomodoro(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires("fmt/11.0.2")
+        self.requires("libassert/2.1.1")
+        self.requires("extra-cmake-modules/6.2.0")
+
+    def layout(self):
+        cmake_layout(self, build_folder="build")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
